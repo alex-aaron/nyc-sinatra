afe298784fb39aa810d3cd789cecf78e2311837e
Alex <alexfrancisaaron@gmail.com>
2021-02-01 11:17:56 -0600

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,9 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures/new' do
+    @figures = Figure.all
+    binding.pry
+    erb :'figures/new'
+  end
 end
